9a759971b184d6fdb6a18daed386a7e9c83ffcf3
Didier-Soldini <soldinididier@wanadoo.fr>
2023-07-13 11:03:08 +0200

    config serveur

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb+srv://Didier:wn0foDG1rwqD6L0j@atlascluster.vp5nwws.mongodb.net/test'
 
 module.exports = async () => {
   try {
