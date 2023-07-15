0fb703a9ca33c53a431870deaf993305aa760950
Didier-Soldini <soldinididier@wanadoo.fr>
2023-07-15 14:43:29 +0200

    page 404 et fichier swagger

--- frontend/src/Pages/PageNotFound.jsx
+++ frontend/src/Pages/PageNotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 const PageNotFound = () => {
   return (
-    <main>
- 
+    <main className="error bg-dark">
+      <h1 className="error__title"><span className="error__title--404">Error: 404</span></h1>
+      <p>The requested page does not exist.</p>
+      <NavLink to="/">Go back to the homepage ?</NavLink>
     </main>
   );
 };
